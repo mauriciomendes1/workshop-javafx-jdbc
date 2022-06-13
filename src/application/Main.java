0ce9b6fab93a389cd4867a65c75f86d4b2a55a7d
mauriciomendes1 <mauriciomendes994@gmail.com>
2022-06-13 19:09:36 -0300

    About view

--- src/application/Main.java
+++ src/application/Main.java
@@ -8,6 +8,9 @@ import javafx.stage.Stage;
 
 
 public class Main extends Application {
+	
+	private static Scene mainScene;
+	
 	@Override
 	public void start(Stage primaryStage) {
 		try {
@@ -18,7 +21,7 @@ public class Main extends Application {
 			 scrollPane.setFitToHeight(true);
 			 scrollPane.setFitToWidth(true);
 			 
-			 Scene mainScene = new Scene(scrollPane);
+			 mainScene = new Scene(scrollPane);
 			 primaryStage.setScene(mainScene);
 			 primaryStage.setTitle("Sample JavaFX application");
 			 primaryStage.show();
@@ -26,6 +29,10 @@ public class Main extends Application {
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
+	}
+	
+	public static Scene getMainScene() {
+		return mainScene;
 	}
 	
 	public static void main(String[] args) {
