b369288b01493f631fbfa951b8c9bf82b324fea7
mauriciomendes1 <mauriciomendes994@gmail.com>
2022-06-15 20:07:24 -0300

    SellerTableView

--- src/gui/SellerListController.java
+++ src/gui/SellerListController.java
@@ -1,6 +1,7 @@
 package gui;
 
 import java.net.URL;
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 import java.util.ResourceBundle;
@@ -33,14 +34,28 @@ public class SellerListController implements Initializable, DataChangeListener {
 
 	@FXML
 	private TableView<Seller> tableViewSeller;
+	
 	@FXML
 	private TableColumn<Seller, Integer> tableColumnId;
+	
 	@FXML
 	private TableColumn<Seller, String> tableColumnName;
+	
+	@FXML
+	private TableColumn<Seller, String> tableColumnEmail;
+	
+	@FXML
+	private TableColumn<Seller, Date> tableColumnBirthDate;
+	
+	@FXML
+	private TableColumn<Seller, Double> tableColumnBaseSalary;
+	
 	@FXML
 	private TableColumn<Seller, Seller> tableColumnEDIT;
+	
 	@FXML
 	private TableColumn<Seller, Seller> tableColumnREMOVE;
+	
 	@FXML
 	private Button btNew;
 
@@ -65,6 +80,11 @@ public class SellerListController implements Initializable, DataChangeListener {
 	private void initializeNodes() {
 		tableColumnId.setCellValueFactory(new PropertyValueFactory<>("id"));
 		tableColumnName.setCellValueFactory(new PropertyValueFactory<>("name"));
+		tableColumnEmail.setCellValueFactory(new PropertyValueFactory<>("email"));
+		tableColumnBirthDate.setCellValueFactory(new PropertyValueFactory<>("birthDate"));
+		Utils.formatTableColumnDate(tableColumnBirthDate, "dd/MM/yyyy");
+		tableColumnBaseSalary.setCellValueFactory(new PropertyValueFactory<>("baseSalary"));
+		Utils.formatTableColumnDouble(tableColumnBaseSalary, 2);
 
 		// COMANDO PRA TABLEVIEW OCUPAR TODA A PARTE DEBAIXO DA JANELA E TB ACOMPANHAR A
 		// MESMA
